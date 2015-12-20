b02f6aadb730e3ab3c685e4cc33df34d642c9cff
victor zhang <victorzhang@vip.163.com>
2015-12-21 02:13:33 +0800

    add function create_node

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+/*链表结点的数据结构定义 */
+typedef struct Node
+{
+	int num;
+	struct Node *next;
+}Node, *LinkList;
+
+//创建一个结点,head 是表的头指针
+LinkList create_node(int num)
+{
+	LinkList p = (LinkList)malloc(sizeof(Node));
+	p->num = num;
+	p->next = NULL;
+	return p;
+}
+
 int main()
 {
 	return 0;
